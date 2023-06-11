57e3c4576d1e2f46cfbd77bea2df8da839d9128e
MrWittman612 <chad.m.wittman75@gmail.com>
2023-06-10 23:48:30 -0500

    generated new day pratice

---
+++ src/day4/BubbleSort.ts
@@ -0,0 +1,11 @@
+export default function bubble_sort(array: number[]): void {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] > array[j + 1]) {
+                const temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
