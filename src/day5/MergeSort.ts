180dc3dba4ea691cdb53db48d0d67319040bf8e5
MrWittman612 <chad.m.wittman75@gmail.com>
2023-06-11 13:53:46 -0500

    created merge_sort but does not pass test ???

--- src/day5/MergeSort.ts
+++ src/day5/MergeSort.ts
@@ -1,3 +1,38 @@
-export default function merge_sort(arr: number[]): void {
+function merge(arr1: number[], arr2: number[]): number[] {
+    let results = [];
+    let i = 0;
+    let j = 0;
 
+    while (i < arr1.length && j < arr2.length) {
+        if (arr2[j] > arr1[i]) {
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        results.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        results.push(arr2[j]);
+        j++;
+    }
+
+    return results;
+}
+
+export default function merge_sort(arr: number[]): number[] {
+    if (arr.length <= 1) return arr;
+
+    let mid = Math.floor(arr.length / 2);
+
+    let left = merge_sort(arr.slice(0, mid));
+    let right = merge_sort(arr.slice(mid));
+
+    return merge(left, right);
 }
