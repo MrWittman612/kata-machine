89b7a2486e65bd3cc9fd254a3d354b0b5b2907fd
MrWittman612 <chad.m.wittman75@gmail.com>
2023-06-16 22:04:57 -0500

    merge sort passed test

---
+++ src/day10/MergeSort.ts
@@ -0,0 +1,38 @@
+function merge(arr1: number[], arr2: number[]): number[] {
+    let result: number[] = [];
+    let arr1Idx = 0;
+    let arr2Idx = 0;
+
+    while (arr1Idx < arr1.length && arr2Idx < arr2.length) {
+        if (arr2[arr2Idx] > arr1[arr1Idx]) {
+            result.push(arr1[arr1Idx]);
+            ++arr1Idx;
+        } else {
+            result.push(arr2[arr2Idx]);
+            ++arr2Idx;
+        }
+    }
+
+    while (arr1Idx < arr1.length) {
+        result.push(arr1[arr1Idx]);
+        ++arr1Idx;
+    }
+
+    while (arr2Idx < arr2.length) {
+        result.push(arr2[arr2Idx]);
+        ++arr2Idx;
+    }
+
+    return result;
+}
+
+export default function merge_sort(arr: number[]): number[] {
+    if (arr.length <= 1) return arr;
+
+    const mid = Math.floor(arr.length / 2);
+
+    const left = merge_sort(arr.slice(0, mid));
+    const right = merge_sort(arr.slice(mid));
+
+    return merge(left, right);
+}
