1c3df0e21dce7f75263afebab85043d634e15f17
MrWittman612 <chad.m.wittman75@gmail.com>
2023-07-04 17:31:39 -0500

    insertion sort test passed

--- src/day13/InsertionSort.ts
+++ src/day13/InsertionSort.ts
@@ -1,3 +1,18 @@
-export default function insertion_sort(arr: number[]): void {
+/*
+ * Big 0 time complexity
+ * log(n): Best case
+ * log(n^2): worst case
+ *
+ */
 
+export default function insertion_sort(arr: number[]): void {
+    for (let i = 1; i < arr.length; i++) {
+        const key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
 }
